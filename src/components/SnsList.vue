61b1ab8b4d18da4315c3fefaf92ec9e84ef93dd2
tamorieeeen <tamorieeeen@gmail.com>
2025-10-31 16:34:14 +0900

    SNSアイコンをSVGに変更、fontawesome fabを削除

--- src/components/SnsList.vue
+++ src/components/SnsList.vue
@@ -5,21 +5,12 @@ import SnsCard from './parts/SnsCard.vue';
 <template>
   <div class="flex-wrapper">
     <SnsCard url="https://x.com/tamorieeeen" colorCode="x-twitter">
-      <template #icon>
-        <font-awesome-icon :icon="['fab', 'square-x-twitter']" />
-      </template>
       X (Twitter)
     </SnsCard>
     <SnsCard url="https://bsky.app/profile/tamorieeeen.bsky.social" colorCode="bluesky">
-      <template #icon>
-        <font-awesome-icon :icon="['fab', 'square-bluesky']" />
-      </template>
       Bluesky
     </SnsCard>
     <SnsCard url="https://www.instagram.com/tamorieeeen/" colorCode="instagram">
-      <template #icon>
-        <font-awesome-icon :icon="['fab', 'instagram']" />
-      </template>
       Instagram
     </SnsCard>
   </div>
--- src/components/parts/SnsCard.vue
+++ src/components/parts/SnsCard.vue
@@ -17,12 +17,14 @@ const color = computed(() => {
     case 'github':
       return {
         rgb: "rgb(14, 12, 13 ,0.1)",
-        hex: "#0E0C0D"
+        hex: "#0E0C0D",
+        filter: "invert(1%) sepia(6%) saturate(2261%) hue-rotate(278deg) brightness(97%) contrast(92%)"
       };
     case 'qiita':
       return {
         rgb: "rgb(85, 197, 0 ,0.1)",
-        hex: "#55C500"
+        hex: "#55C500",
+        filter: "invert(70%) sepia(51%) saturate(3943%) hue-rotate(52deg) brightness(96%) contrast(103%)"
       };
     case 'x-twitter':
       return {
@@ -32,41 +34,48 @@ const color = computed(() => {
     case 'bluesky':
       return {
         rgb: "rgb(0, 133, 255, 0.1)",
-        hex: "#0085FF"
+        hex: "#0085FF",
+        filter: "invert(32%) sepia(76%) saturate(2025%) hue-rotate(194deg) brightness(102%) contrast(103%)"
       };
     case 'instagram':
       return {
         rgb: "rgb(207, 46, 146, 0.1)",
-        hex: "#CF2E92"
+        hex: "#CF2E92",
+        filter: "invert(26%) sepia(98%) saturate(2134%) hue-rotate(304deg) brightness(85%) contrast(88%)"
       };
   }
 });
+const imgUrl = new URL(`../../assets/icon/${props.colorCode}.svg`, import.meta.url).href;
 </script>
 
 <template>
   <div class="card-box">
     <a :href="url" target="_blank" rel="noopener">
-      <span class="sns-icon"><slot name="icon"></slot></span>
-      <span class="sns-name"><slot></slot></span>
+      <img :alt="colorCode" :src="imgUrl" :class="colorCode" />
+      <div class="sns-name"><slot></slot></div>
     </a>
   </div>
 </template>
 
 <style scoped>
-a {
+.card-box {
+  border: 2px solid v-bind('color.hex');
+  border-radius: 10px;
+}
+.card-box > a {
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 10px;
-}
-.card-box {
   width: 150px;
-  border: 2px solid v-bind('color.hex');
-  border-radius: 10px;
+  height: 50px;
 }
-.sns-icon {
-  font-size: 30px;
-  color: v-bind('color.hex');
+.card-box img:not(.x-twitter) {
+  width: 28px;
+  filter: v-bind('color.filter');
+}
+.card-box img.x-twitter {
+  width: 32px;
 }
 .sns-name {
   font-size: 1.1rem;
@@ -78,7 +87,7 @@ a {
   }
 }
 @media (min-width: 768px) {
-  .card-box {
+  .card-box > a {
     width: 170px;
   }
 }
